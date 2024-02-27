3a57699ccd532337dd65ff056178bd68dbe3571d
changhyun-Hong <alex963@naver.com>
2024-02-28 02:28:41 +0900

    feat: tailwindcss 오류해결

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import './App.css';
-import styled from 'styled-components';
+import React from 'react';
 import Poll from './pages/Poll';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <RootContainer>
-            <InnerContainer>
+        <div className='flex h-screen bg-white justify-center'>
+            <div className='w-1/4 min-w-350 bg-white'>
                 <Routes>
                     <Route path='/poll' element={<Poll />} />
                 </Routes>
-            </InnerContainer>
-        </RootContainer>
+            </div>
+        </div>
     );
 }
 
 export default App;
-
-const RootContainer = styled.div`
-    display: flex;
-    height: 100vh;
-    background-color: white;
-    // background-color: #e9e9e9;
-    justify-content: center;
-    margin: 0 auto;
-    flex: 1;
-`;
-
-const InnerContainer = styled.div`
-    width: 25%;
-    min-width: 350px;
-    background-color: white;
-`;
--- src/pages/Poll.js
+++ src/pages/Poll.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import InputLine from '../component/InputLine';
-import styled from 'styled-components';
 
 export default function Poll() {
     return (
-        <PollContainer>
-            <TitleBox>걸엉가게</TitleBox>
-            <TextBox>가고싶은 올레길을 알려주세요</TextBox>
-            <InputLine placeholder={'#바다가보이는'} />
-            <TextBox>먹고싶은 음식을 알려주세요</TextBox>
-            <InputLine placeholder={'#돼지고기'} />
-        </PollContainer>
+        <div className='flex flex-col m-4'>
+            <div className='flex text-xl text-green-600 mb-40 mt-8'>
+                걸엉가게
+            </div>
+            <div className='mb-4 mt-4'>가고싶은 올레길을 알려주세요</div>
+            <InputLine
+                placeholder='#바다가보이는'
+            />
+            <div className='mb-4 mt-4'>먹고싶은 음식을 알려주세요</div>
+            <InputLine
+                placeholder='#돼지고기'
+            />
+        </div>
     );
 }
-
-const PollContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 1rem;
-`;
-
-const TitleBox = styled.div`
-    display: flex;
-    color: #555f1a;
-    font-size: x-large;
-    margin-bottom: 10rem;
-    margin-top: 2rem;
-`;
-
-const TextBox = styled.div`
-    margin: 1rem 0;
-`;
