7267c90b7f33a08122830ae753889057ead867b1
kimkun07 <kimkun07@naver.com>
2024-02-28 01:52:36 +0900

    feat: Fetch

---
+++ src/FetchAndLoading.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const FetchAndLoading = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Make the server request here
+      // Replace the URL with your actual server endpoint
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+
+      // Once the response is received, stop showing the loading bar
+      setIsLoading(false);
+
+      // Do something with the data
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading ? "Loading" : children;
+};
+
+export default FetchAndLoading;
--- src/components/myButton.jsx
+++ src/components/myButton.jsx
@@ -4,7 +4,7 @@ const MyButton = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="w-full h-full m-auto rounded bg-red-500 text-white"
+      className="w-full h-full m-auto rounded p-2 bg-[#555F1A] text-white"
     >
       {text}
     </button>
