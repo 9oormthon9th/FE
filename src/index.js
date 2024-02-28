f9bd5eea4b89c46a0068708e84e7046b10360a4e
kimkun07 <kimkun07@naver.com>
2024-02-29 08:36:11 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 "use client";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 serviceWorkerRegistration.register();
 reportWebVitals();
 
-var my_awesome_script = document.createElement("script");
-
-my_awesome_script.setAttribute("src", "//dapi.kakao.com/v2/maps/sdk.js?appkey=2ca3e874ab120943d901e338daef61ff&libraries=services,clusterer");
-
-document.head.appendChild(my_awesome_script);
+const script = document.createElement("script");
+script.type = "text/javascript";
+script.src =
+  "//dapi.kakao.com/v2/maps/sdk.js?appkey=2ca3e874ab120943d901e338daef61ff&libraries=services,clusterer";
+script.async = true;
+script.dataset.cfasync = false;
+document.body.appendChild(script);
+script.addEventListener("load", () => {
+  console.log("Script added successfully");
+//   resolve();
+});
+console.log("Script added");
