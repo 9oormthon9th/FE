0b291eff487c5762919f3a07f9013b6d466f12eb
kimkun07 <kimkun07@naver.com>
2024-02-29 08:48:26 +0900

    index to App

--- src/index.js
+++ src/index.js
@@ -17,16 +17,3 @@ root.render(
 
 serviceWorkerRegistration.register();
 reportWebVitals();
-
-const script = document.createElement("script");
-script.type = "text/javascript";
-script.src =
-  "//dapi.kakao.com/v2/maps/sdk.js?appkey=2ca3e874ab120943d901e338daef61ff&libraries=services,clusterer";
-script.async = true;
-script.dataset.cfasync = false;
-document.body.appendChild(script);
-script.addEventListener("load", () => {
-  console.log("Script added successfully");
-//   resolve();
-});
-console.log("Script added");
