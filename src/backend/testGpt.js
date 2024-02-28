ba9baff97564dd2ce0cbcf06f7db27b02dab1862
kimkun07 <kimkun07@naver.com>
2024-02-29 02:52:24 +0900

    request: banned because of no-cors

--- src/backend/testGpt.js
+++ src/backend/testGpt.js
@@ -1,19 +1,23 @@
-const { hostname } = window.location;
-
-const ready = async () => {
-  console.log(hostname);
+const testGpt = async (theme) => {
+  const url = `https://k7c2e2155cca0a.user-app.krampoline.com/api/ready`;
+  console.log(url);
+  
   try {
     // Make a request to the server to get the image
-    const response = await fetch(`https://${hostname}:5000/api/ready`, {
-      method: "GET",
-      // Add any necessary headers or parameters
-    });
-    
+    const response = await fetch(
+      url,
+      {
+        method: "GET",
+      }
+    );
+
     console.log(response);
 
     // Check if the request was successful
     if (response.ok) {
       // Parse the response as an image or do any necessary processing
+      var x = await response.body;
+      console.log(x);
     } else {
       // Handle the error if the request was not successful
       throw new Error("Failed to get image from server");
@@ -23,6 +27,6 @@ const ready = async () => {
     console.error(error);
     throw error;
   }
-}
+};
 
-export default ready;
+export default testGpt;
--- src/pages/Preview.js
+++ src/pages/Preview.js
@@ -1,10 +1,11 @@
 import ready from "../backend/ready";
+import testGpt from "../backend/testGpt";
 import MyButton from "../component/MyButton";
 import { currentPos } from "../mocks/currentPos";
 import ResultPage from "./ResultPage";
 
 export default function Preview() {
-  ready();
+  testGpt("바다");
   return (
     <>PREIVEW</>
     // <ResultPage centerPos={currentPos} />
