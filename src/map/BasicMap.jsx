cf444fe34aff0ee232e780245dfcc48ba8bf42af
kimkun07 <kimkun07@naver.com>
2024-02-28 11:23:57 +0900

    feat: marker

--- src/map/BasicMap.jsx
+++ src/map/BasicMap.jsx
@@ -1,23 +1,25 @@
-import { Map } from "react-kakao-maps-sdk";
-import { useKakaoLoader } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useState } from "react";
 
-export default function BasicMap() {
-  // const [ loading, error ] = useKakaoLoader();
+export default function BasicMap({places}) {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Map // 지도를 표시할 Container
-      id="map"
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
+    <Map
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{ width: "100%", height: "360px" }}
+      onClick={(_, mouseEvent) => {
+        setIsOpen(false);
       }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "350px",
-      }}
-      level={3} // 지도의 확대 레벨
-    />
+    >
+      {places.map((place, index) => (
+        <MapMarker
+          key={place.title}
+          position={{ lat: place.lat, lng: place.lng }}
+          // clickable={true}
+          // onClick={() => setIsOpen(true)}
+        />
+      ))}
+    </Map>
   );
 }
