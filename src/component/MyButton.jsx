0e10dcf20d368a940583c03ffdd86d847e26ed51
kimkun07 <kimkun07@naver.com>
2024-02-28 10:37:34 +0900

    Update MyButton.jsx

--- src/component/MyButton.jsx
+++ src/component/MyButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {primaryColor} from "../theme/color";
 
-const MyButton = ({ text, onClick }) => {
+const MyButton = ({ text, onClick, disabled = false }) => {
   return (
     <button
+      disabled={disabled ? true : false}
       onClick={onClick}
-      className={`w-full h-full m-auto rounded p-2 bg-[${primaryColor}] text-white`}
+      className={`w-full h-full m-auto rounded p-2 bg-[${primaryColor}] text-white disabled:opacity-50`}
     >
       {text}
     </button>
