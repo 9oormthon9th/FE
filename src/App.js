2ec6c037bbd91ac87d6215ceb8db0b404064707e
kimkun07 <kimkun07@naver.com>
2024-02-28 09:36:30 +0900

    Update App.js - responsive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Poll from './pages/Poll';
 import { Routes, Route } from 'react-router-dom';
+import Preview from './pages/Preview';
 
 function App() {
     return (
-        <div className='flex h-screen bg-white justify-center'>
-            <div className='w-1/4 min-w-350 bg-white'>
-                <Routes>
-                    <Route path='/poll' element={<Poll />} />
-                </Routes>
-            </div>
+      <div className="flex h-screen bg-purple-500 justify-center">
+        <div className="w-full md:w-[768px] bg-white">
+          <Routes>
+            <Route path="/poll" element={<Poll />} />
+            <Route path="/preview" element={<Preview />} />
+          </Routes>
         </div>
+      </div>
     );
 }
 
