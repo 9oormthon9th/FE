f9cb1fa5a9647bd4454b88f8e2de516944be7046
kimkun07 <kimkun07@naver.com>
2024-02-29 00:54:51 +0900

    fetch backend

---
+++ src/backend/ready.js
@@ -0,0 +1,28 @@
+const { hostname } = window.location;
+
+const ready = async () => {
+  console.log(hostname);
+  try {
+    // Make a request to the server to get the image
+    const response = await fetch(`http://${hostname}:5000/api/ready`, {
+      method: "GET",
+      // Add any necessary headers or parameters
+    });
+    
+    console.log(response);
+
+    // Check if the request was successful
+    if (response.ok) {
+      // Parse the response as an image or do any necessary processing
+    } else {
+      // Handle the error if the request was not successful
+      throw new Error("Failed to get image from server");
+    }
+  } catch (error) {
+    // Handle any other errors that may occur
+    console.error(error);
+    throw error;
+  }
+}
+
+export default ready;
--- src/pages/Preview.js
+++ src/pages/Preview.js
@@ -1,9 +1,12 @@
+import ready from "../backend/ready";
 import MyButton from "../component/MyButton";
 import { currentPos } from "../mocks/currentPos";
 import ResultPage from "./ResultPage";
 
 export default function Preview() {
+  ready();
   return (
-    <ResultPage centerPos={currentPos} />
+    <>PREIVEW</>
+    // <ResultPage centerPos={currentPos} />
   );
 }
