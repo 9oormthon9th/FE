d8ef11e17ef85073899dccb5024d0ce03de9bd4c
kimkun07 <kimkun07@naver.com>
2024-03-03 15:09:30 +0900

    Chore: Use strict mode

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import { StrictMode } from "react";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 
 serviceWorkerRegistration.register();
